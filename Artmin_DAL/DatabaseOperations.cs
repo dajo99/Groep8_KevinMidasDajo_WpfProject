5036b6a334711214441447816acf074e9bf46fdc
Midas Wouters <midas.wouters@gmail.com>
2020-06-15 15:53:48 +0200

    Created basic events page

--- Artmin_DAL/DatabaseOperations.cs
+++ Artmin_DAL/DatabaseOperations.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,15 @@ namespace Artmin_DAL
 {
     public static class DatabaseOperations
     {
+        public static List<Event> GetEvents()
+        {
+            using (var entities = new ArtminEntities())
+            {
+                return entities.Events
+                            .OrderByDescending(e => e.Date)
+                            .Include(e => e.EventType)
+                            .ToList();
+            }
+        }
     }
 }
---
+++ Artmin_DAL/Partials/EventType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Artmin_DAL
+{
+    public partial class EventType
+    {
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
