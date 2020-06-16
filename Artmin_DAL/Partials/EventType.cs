77dd1e94b0098b5ffeb8df027dc0550a4d8aaed1
Midas Wouters <midas.wouters@gmail.com>
2020-06-16 20:38:49 +0200

    Added ColorSwatchConverter class + added ColorName property to EventType model

--- Artmin_DAL/Partials/EventType.cs
+++ Artmin_DAL/Partials/EventType.cs
@@ -8,6 +8,21 @@ namespace Artmin_DAL
 {
     public partial class EventType
     {
+        private static readonly Dictionary<string, string> EventColors = new Dictionary<string, string>
+        {
+            { "Fuif",       "Green" },
+            { "Trouwfeest", "Yellow" },
+            { "VAT",        "Red" },
+            { "DJ SET",     "Blue" }
+        };
+        public string ColorName
+        {
+            get
+            {
+                return EventColors.TryGetValue(Name, out string colorName) ? colorName : "Grey";
+            }
+        }
+
         public override string ToString()
         {
             return Name;
